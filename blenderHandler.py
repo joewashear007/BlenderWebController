ffbde11b7058994dafef8ce6bc697dd006e2609a
Joseph Livecchi <joewashear007@gmail.com>
2014-01-14 12:16:04 -0500

    Started blender server

--- blenderHandler.py
+++ blenderHandler.py
@@ -8,26 +8,12 @@
  # * this stuff is worth it, you can buy me a beer in return Joseph Livecchi
  # * ----------------------------------------------------------------------------
  # */
- 
-from server import WebSocketHttpServer
-from server import WebSocketsHandler
 
-def main():
-	print()
-	print()
-	print("*** Starting Websocket Server ***")
-	print()
-	print("Press Any Key To Quit...")
-	print()
-	server = WebSocketHttpServer(8000, 9999, WebSocketsHandler)
-	if server.start():
-		print()
-		server.launch_webpage()
-	else:
-		print("Error Starting Server")
-	i = input()
-	server.stop()
-	print("Good Bye")
+import server
 
-if __name__ == '__main__':
-	main()
+class BlenderHandler(WebSocketsHandler):
+    def on_message(self, msg):
+        pass
+        
+    def send_message(self, msg):
+        pass
