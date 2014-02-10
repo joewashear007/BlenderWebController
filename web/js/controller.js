28be196ea4baf5d86ae611ead7f1a588ad9020e1
Joseph Livecchi <joewashear007@gmail.com>
2014-02-10 10:51:24 -0500

    Cleaned JS code
    Closed #35
    Started on #36

--- web/js/controller.js
+++ web/js/controller.js
@@ -6,9 +6,15 @@
  * this stuff is worth it, you can buy me a beer in return Joseph Livecchi
  * ----------------------------------------------------------------------------
  */
- 
+
+/* -------------------------- Global Varibles ---------------------------- */ 
 Hammer.plugins.fakeMultitouch();
 var s = null;
+
+/* -------------------------- Websocket Fucntions ---------------------------- 
+ Functions used to handel the connection, opening, closing sendgin, and reciveing of the websocket
+*/ 
+
 function open (e) 	{ 
 	$("#DebugMsgList").append("<p>Connect!</p>"); 
 	$("#ToggleCxnStatus").removeClass("danger"); 
@@ -17,23 +23,35 @@ function open (e) 	{
 function close(e) 	{ 
 	$("#DebugMsgList").append("<p>Connection Closed!</p>"); 
 	$("#ToggleCxnStatus").addClass("danger"); 
-	$("#ErrMsg").fadeIn();
+	$("#ErrMsg").text("Not Connected").fadeIn();
 };
 function msg  (e) 	{ 
-	$("#DebugMsgList").append("<p>Received: "+ e.data + "</p>"); 
+	$("#DebugMsgList").append("<p>Received: "+ e.data + "</p>");
 };
 function error(e)  	{ 
 	$("#DebugMsgList").append("<p>Error: "+e+"</p>"); 
 	$("#ToggleCxnStatus").addClass("danger"); 
-	$("#ErrMsg").text("Err!").fadeIn();	
+	$("#ErrMsg").text("Error! - Check Msg").fadeIn();	
 };
+function send(key,msg){
+    var raw_data = {};
+    raw_data[key] = msg;
+    var data = JSON.stringify(raw_data);
+    if(s){
+       s.send(data);
+    }else{
+        $("#DebugMsgList").append("<p>Event: "+ data + "</p>");
+    }
+}
 function toggleConnection(){
+    //Opens and closes a conenction using the address in #CxnWSAddress
 	if(s){
 		s.close(1000, "Try to Close");
 		$("#ToggleCxnBtn").text("Connect");
 		s = null;
 	}else{
 		try {
+            address = $("#CxnWSAddress").val();
 			if (address != "" ||  address != "$address") {
 				s = new WebSocket(address);
 				s.onopen = open;
@@ -43,27 +61,24 @@ function toggleConnection(){
 				$("#ToggleCxnBtn").text("Disconnect");
 			}
 		} catch (ex) {
-			error(ex);
+			error("Could Not Connected");
 		}
 	}
 }
 
+/* -------------------------- Document Ready Function ---------------------------- 
+ Main function, Handels all events, swipe, and keybord control
+ Makes the QR code
+*/ 
 
 $(document).ready(function(){
     $("#CxnHTTPAddress").val(document.URL);
     $("#CxnWSAddress").val(address);
 	toggleConnection();
 	
-	
     var qrcode = new QRCode(document.getElementById("CxnQR"), document.URL);
     
-    $(".ctrlBtn").mousedown( function () {
-        var data = {"Actuator":$(this).attr("id"), };
-        s.send(JSON.stringify(data));
-    }).mouseup(function(){
-		var data = {"Stop":"All", };
-        s.send(JSON.stringify(data));
-	});
+    // ---------------- UI Button Events ---------------------------------
     $("#ToggleCxnStatus").click( function() { 
         $("#PopWrapHead > h3").text("Cxn Status");
         $("#DebugInfo").hide();
@@ -76,33 +91,52 @@ $(document).ready(function(){
         $("#CxnStatus").hide();
         $("#PopWrap").fadeIn();
     });
-    $("#ResetModel").mouseup(function() { s.send(JSON.stringify({"Reset":true}));});
-    $("#PopWrapCloseBtn").click(function(){ $("#PopWrap").fadeOut(); });
-    $("#DebugMsgListBtn").click(function(){ $("#DebugMsgList").empty(); });
-    $("#ToggleCxnBtn").click( toggleConnection );
-    $("#ToggleControl").click( function() {
+    $("#ToggleCxnBtn")   .click( toggleConnection );
+    $("#PopWrapCloseBtn").click( function() { $("#PopWrap").fadeOut();      });
+    $("#DebugMsgListBtn").click( function() { $("#DebugMsgList").empty();   });
+    $("#ToggleControl")  .click( function() {
         $("#SwipeControl").fadeToggle();
         $("#ButtonControl").fadeToggle();
         $(this).text();
     });
-
-    // // start!
+    
+    //------------------ Control events ----------------------------------
+    
+    //Arrow Button click event
+    $(".ctrlBtn").mousedown(    function() { send("Actuator", $(this).attr("id"));  })
+                 .mouseup  (    function() { send("Stop", "All");                   });
+    // Reset Button
+    $("#ResetModel").click (    function() { s.send(JSON.stringify({"Reset":true}));});
+    
+    // Swipe Control
     var element = document.getElementById('SwipeControl');
     var hammertime = Hammer(element, {
             prevent_default: true,
             no_mouseevents: true
     })
-    .on("tap",          function(event) { $("#SwipeEvent").text("tap");                                 })
-    .on("dragleft",     function(event) { s.send(JSON.stringify({"Actuator":"RotateLeft", }));          })
-    .on("dragright",    function(event) { s.send(JSON.stringify({"Actuator":"RotateRight", }));         })
-    .on("dragdown",     function(event) { s.send(JSON.stringify({"Actuator":"RotateDown", }));          })
-    .on("dragup",       function(event) { s.send(JSON.stringify({"Actuator":"RotateUp", }));            })
-    .on("pinchin",      function(event) { s.send(JSON.stringify({"Actuator":"ZoomIn", }));              })
-    .on("pinchout",     function(event) { s.send(JSON.stringify({"Actuator":"ZoomOut", }));             })
-    .on("rotate",       function(event) { s.send(JSON.stringify({"Actuator":"ZRotateLeft", }));         })
-    .on("release",      function(event) { s.send(JSON.stringify({"Stop":"All", }));                     });
+    .on("tap",          function(event) { send("Stop"    , "All"            );  })
+    .on("dragleft",     function(event) { send("Actuator", "RotateLeft"     );  })
+    .on("dragright",    function(event) { send("Actuator", "RotateRight"    );  })
+    .on("dragdown",     function(event) { send("Actuator", "RotateDown"     );  })
+    .on("dragup",       function(event) { send("Actuator", "RotateUp"       );  })
+    .on("pinchin",      function(event) { send("Actuator", "ZoomIn"         );  })
+    .on("pinchout",     function(event) { send("Actuator", "ZoomOut"        );  })
+    .on("rotate",       function(event) { send("Actuator", "ZRotateLeft"    );  })
+    .on("release",      function(event) { send("Stop"    , "All"            );  });
+    
+    //Keyboard Events
+    $(document).keydown( function(event) {
+        //Left Arrow
+        if ( event.which == 37 ) { event.preventDefault(); send("Actuator", "RotateLeft" ); }
+        // Up Arrow 
+        if ( event.which == 38 ) { event.preventDefault(); send("Actuator", "RotateUp" ); }
+        //Right Arrow
+        if ( event.which == 39 ) { event.preventDefault(); send("Actuator", "RotateRight" ); }
+        //Down Arrow    
+        if ( event.which == 40 ) { event.preventDefault(); send("Actuator", "RotateDown" ); }
 
-
+    });
+    $(document).keyup( function(event) { send("Stop", "All");});
 });
 
 
